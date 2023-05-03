e779656b9990dbff30fd3587d42ff37ed0bf5060
(W) Cauê Carrera <cauecpl@gmail.com>
2023-05-03 11:16:04 -0300

    Starting setup for coding

---
+++ src/assets/js/script.js
@@ -0,0 +1,5 @@
+// import contents from modules
+import 'reset-css';
+import '@mdi/font/css/materialdesignicons.min.css';
+
+// import personal contents
---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    main: [
+      './src/assets/js/script.js',
+    ],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'assets/js/[name].js',
+    assetModuleFilename: 'assets/images/[name][ext][query]',
+    // clean: true,
+  },
+
+  optimization: {
+    moduleIds: 'deterministic',
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+        styles: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          type: 'css/mini-extract',
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: 'all',
+      title: "~we don't talk about bruno~",
+      filename: 'index.html',
+      template: './src/index.html',
+      inject: 'body',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/[name].css',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          // 2. remove CSS from JS and save into an external file
+          { loader: MiniCssExtractPlugin.loader },
+          // 1. generate CSS into CommonJS
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          // 3. remove CSS from JS and save into an external file
+          MiniCssExtractPlugin.loader,
+          // 2. generate CSS into CommonJS
+          'css-loader',
+          // 1. tranpile SCSS into CSS
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require.resolve('sass'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
